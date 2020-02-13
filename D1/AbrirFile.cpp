98ddeab63a173b1c09f58474e73528b54b2ba0c4
Yagyu27 <arnovisdee@hotmail.com>
2020-02-13 20:56:07 +0100

    Update

--- D1/AbrirFile.cpp
+++ D1/AbrirFile.cpp
@@ -8,8 +8,8 @@ int main()
 
 {
 	cout<<"\nAbriendo A5.txt";
-	//system("notepad.exe  C:\\D3\\D31\\A5.txt");
-	system("notepad.exe  C:\\Users\\Arnovis\\Documents\\UTB\\Sistemas Operativos\\CE-SistemasOp\\D3\\D31\\A5.txt"); 
+	system("notepad.exe  C:\\D3\\D31\\A5.txt");
+	//system("notepad.exe  C:\\Users\\Arnovis\\Documents\\UTB\\Sistemas Operativos\\CE-SistemasOp\\D3\\D31\\A5.txt"); 
 	cout<<"\nCerrando :c";
 	return 0;
 
