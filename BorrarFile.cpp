03045de5b58ba78ffe3bfc5aec30af15f8988c5e
Yagyu27 <arnovisdee@hotmail.com>
2020-02-13 20:46:22 +0100

    Create Drop File and Update Open File

---
+++ BorrarFile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+main(){
+	/*
+	if(remove("C:\\Users\\Arnovis\\Documents\\UTB\\Sistemas Operativos\\CE-SistemasOp\\prueba.txt") != 0 )
+    	cout<<"Error al borrar el archivo.\n";
+  	else
+    	cout<<"\nArchivo borrado con exito!.";
+  return 0; */
+  	if(remove("C:\\A1.txt") != 0 )
+    	cout<<"Error al borrar el archivo.\n";
+  	else
+    	cout<<"\nArchivo borrado con exito!.";
+  	return 0;
+  
+}
